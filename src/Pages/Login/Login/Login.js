eb58050d627e8f044dfef2436e5a7a4a7061974a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-11-14 16:42:19 +0600

    login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const handleLogin = data => {
         console.log(data);
     }
@@ -18,13 +18,22 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text" required {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="text"
+                            {...register("email", { required: "Email Address is required" })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" required {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password"
+                            required {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: "Password must be at least 6 character" }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                         <label className="label">
                             <span className="label-text"> forgot password</span>
                         </label>
